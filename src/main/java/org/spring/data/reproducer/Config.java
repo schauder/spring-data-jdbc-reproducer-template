c749802e9df2aa882a79512e25987684306e9a7e
Jens Schauder <jschauder@vmware.com>
2021-06-11 12:57:59 +0200

    ApplicationContext works.

---
+++ src/main/java/org/spring/data/reproducer/Config.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.spring.data.reproducer;
+
+import org.postgresql.ds.PGSimpleDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jdbc.repository.config.AbstractJdbcConfiguration;
+import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableJdbcRepositories
+public class Config extends AbstractJdbcConfiguration {
+
+	@Bean
+	DataSource dataSource() {
+
+		PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer("postgres:11.1")
+				.withDatabaseName("integration-tests-db")
+				.withUsername("user")
+				.withPassword("password");
+
+		postgreSQLContainer.start();
+
+		PGSimpleDataSource dataSource = new PGSimpleDataSource();
+		dataSource.setURL(postgreSQLContainer.getJdbcUrl());
+		dataSource.setUser(postgreSQLContainer.getUsername());
+		dataSource.setPassword(postgreSQLContainer.getPassword());
+
+		return dataSource;
+	}
+
+	@Bean
+	NamedParameterJdbcTemplate namedParameterJdbcTemplate(DataSource dataSource) {
+		return new NamedParameterJdbcTemplate(dataSource);
+	}
+}
